de680351521c0f0a14236eae146fca8527f3a28a
Viet Khuong <vietkhuong2603@gmail.com>
2022-04-21 16:01:30 +0700

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,3 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 2;
+export const KEY = '50845db3-01d1-4487-8109-6996f0451552';
---
+++ src/js/controller.js
@@ -0,0 +1,136 @@
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+import * as model from './model';
+
+import recipeView from './views/recipeView';
+import searchView from './views/searchView';
+import resultsView from './views/resultsView';
+import bookmarksView from './views/bookmarksView';
+import paginationView from './views/paginationView';
+import addRecipeView from './views/addRecipeView';
+
+// https://forkify-api.herokuapp.com/v2
+
+// if (module.hot) {
+//   module.hot.accept();
+// }
+
+const controlRecipes = async function () {
+  try {
+    const id = window.location.hash.slice(1);
+
+    if (!id) return;
+    recipeView.renderSpinner();
+
+    // Update results view to mark selected search result
+    resultsView.update(model.getSearchResultsPage());
+    // Updating bookmark
+    bookmarksView.update(model.state.bookmarks);
+
+    // loading recipe
+    await model.loadRecipe(id);
+
+    // redering recipe
+    recipeView.render(model.state.recipe);
+  } catch (error) {
+    recipeView.renderError();
+  }
+};
+
+const controlSearchResult = async function () {
+  try {
+    resultsView.renderSpinner();
+
+    // Get search query
+    const query = searchView.getQuery();
+    if (!query) return;
+
+    // Load  search results
+    await model.loadSearchResult(query);
+
+    // Render results
+    // console.log(model.state.search.results);
+    resultsView.render(model.getSearchResultsPage());
+
+    // Render initial pagination button
+    paginationView.render(model.state.search);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const controlPagination = function (gotoPage) {
+  // Render new results
+  // console.log(model.state.search.results);
+  resultsView.render(model.getSearchResultsPage(gotoPage));
+
+  // Render new initial pagination button
+  paginationView.render(model.state.search);
+};
+
+const controlServings = function (newServings) {
+  // Update recipe servings (in state)
+  model.updateServings(newServings);
+  // Update the recipe new
+  // recipeView.render(model.state.recipe);
+  recipeView.update(model.state.recipe);
+};
+
+const controlAddBookmark = function () {
+  // Add/remove bookmark
+  if (!model.state.recipe.bookmarked) model.addBookmark(model.state.recipe);
+  else model.deleteBookmark(model.state.recipe.id);
+
+  // Update recipe view
+  recipeView.update(model.state.recipe);
+
+  // Render bookmarks
+  bookmarksView.render(model.state.bookmarks);
+};
+
+const controlBookmarks = function () {
+  bookmarksView.render(model.state.bookmarks);
+};
+
+const controlAddRecipe = async function (newRecipe) {
+  try {
+    // Show loading spinner
+    addRecipeView.renderSpinner();
+
+    // Upload Recipe data
+    await model.uploadRecipe(newRecipe);
+    console.log(model.state.recipe);
+
+    // render recipe
+    recipeView.render(model.state.recipe);
+
+    // success message
+    addRecipeView.renderMessage();
+
+    // render bookmark view
+    bookmarksView.render(model.state.bookmarks);
+
+    // change ID in URL
+    window.history.pushState(null, '', `#${model.state.recipe.id}`);
+    // window.history.back();
+
+    // close form create Recipe
+    setTimeout(function () {
+      addRecipeView.toggleWindow();
+    }, 1500);
+  } catch (error) {
+    addRecipeView.renderError(error.message);
+  }
+};
+
+const init = function () {
+  bookmarksView.addHandlerRender(controlBookmarks);
+  recipeView.addHandlerRender(controlRecipes);
+  recipeView.addHandlerUpdateServings(controlServings);
+  recipeView.addHandlerAddBookmark(controlAddBookmark);
+  searchView.addHandlerSearch(controlSearchResult);
+  paginationView.addHandlerClick(controlPagination);
+  addRecipeView.addHandlerUpload(controlAddRecipe);
+};
+
+init();
